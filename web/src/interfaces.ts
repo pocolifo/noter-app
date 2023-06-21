f7bf4ae46b6236a78512ae0f35cdd2f703bf2da6
vimpro <90940950+vimpro@users.noreply.github.com>
2023-06-21 18:12:14 +0200

    added components for note content types: - headers - text - images (will add diagrams, vocab, and tables later)
    also put note structures into interfaces.ts

--- web/src/interfaces.ts
+++ web/src/interfaces.ts
@@ -1,3 +1,13 @@
 export interface NavItemProps {
     title: string;
 }
+
+export interface NoteData {
+    Title: string,
+    Content: ContentBlock[]
+}
+
+export interface ContentBlock {
+    Type: string, // as of the moment this is one of the following: text, image, header, table, vocabulary, diagram
+    Data: object
+}
